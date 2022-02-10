71673a9959c8b560ee33aeae23f20adfb80d1a03
Unzor <71789103+Unzor@users.noreply.github.com>
2022-02-09 20:02:44 -0600

    Create read.js

---
+++ read.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+var { open } = require("openurl")
+var image = "data:image/png;base64," + Buffer.from(fs.readFileSync(process.argv[2])).toString('base64');
+var Tesseract = require('tesseract.js');
+
+Tesseract.recognize(
+  image,
+  'eng'
+).then(({ data: { text } }) => {
+  console.log("Visiting: https://cutlify.seven7four4.repl.co/" + text)
+  open("https://cutlify.seven7four4.repl.co/" + text);
+})
