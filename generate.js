85ff0a074fa00ddc389bdeb4716cf001bb234290
Unzor <71789103+Unzor@users.noreply.github.com>
2022-02-09 19:56:52 -0600

    Create generate.js

---
+++ generate.js
@@ -0,0 +1,72 @@
+function cutHex(h){
+	return (h.charAt(0)=="#") ? h.substring(1,7):h
+}
+
+function rgbToHex(){
+var rgb = Array.from(arguments).join(", ");
+return '#' + rgb.split(',').map(x => (+x).toString(16).padStart(2,0)).join('')
+}
+
+function hexToRgb(h){
+	return [parseInt((cutHex((h.charAt(0)=="#") ? h.substring(1,7):h)).substring(0,2),16), parseInt((cutHex((h.charAt(0)=="#") ? h.substring(1,7):h)).substring(2,4), 16), parseInt((cutHex((h.charAt(0)=="#") ? h.substring(1,7):h)).substring(4,6),16)]
+}
+
+
+const fetch = require('node-fetch');
+
+async function shorten_url(u) {
+ var r = await fetch("https://cutlify.seven7four4.repl.co/api/shorten", {
+method: "POST",
+headers: {
+"Content-Type": "application/json"
+},
+body: JSON.stringify({old: u})
+});
+  r = await r.json()
+  return await r.url;
+}
+
+var PImage = require("pureimage")
+var fs = require("fs");
+const randomColor = () => {
+   let color = '#';
+   for (let i = 0; i < 6; i++){
+      const random = Math.random();
+      const bit = (random * 16) | 0;
+      color += (bit).toString(16);
+   };
+   return color;
+};
+
+var c = randomColor()
+
+    var fnt = PImage.registerFont('trebuc.ttf','Trebuchet');
+    fnt.load(async () => {
+      var code = await shorten_url(process.argv[2]);
+      console.log(code);
+        var img = PImage.make(200,200);
+        var ctx = img.getContext('2d');
+      console.log(c, c.length)
+      if (c.length > 7) {
+c = c.slice(0, -1)
+}
+            console.log(c, c.length)
+
+      // fill with red
+ctx.fillStyle = c;
+ctx.fillRect(0,0,200,200);
+           if (c.length > 7) {
+c = c.slice(0, -1)
+}
+                  console.log(c, c.length)
+        ctx.fillStyle = rgbToHex([hexToRgb(c)[0] + 50, hexToRgb(c)[1] + 50, hexToRgb(c)[2] + 50]);
+        ctx.font = "17px 'Source Sans Pro'";
+        ctx.fillText(code, 50, 100);
+
+      //write to 'out.png'
+PImage.encodePNGToStream(img, fs.createWriteStream(process.argv[3])).then(() => {
+    console.log("wrote out the png file to " + process.argv[3] + ".png");
+}).catch((e)=>{
+    console.log("there was an error writing");
+});
+    });
